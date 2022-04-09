1b8397a53d07ad35a4444631dc9aa40a8a0e3c67
ysaad01 <ysaad2325@gmail.com>
2022-04-09 16:53:41 -0400

    added additional readme section object questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,27 +1,61 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
 const fs = require('fs')
+const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [
   {
-    message: "What is the title of your project?",
+    message: "What is the title of your project? (Required)",
     type: "input",
     name: "title",
   },
-
   {
-    message: "Provide a description of the project:",
+    message: "Enter your GitHub username. (Required)",
+    type: "input",
+    name: "github",
+  },
+  {
+    message: "Enter the Github URL to your project. (Required)",
+    type: "input",
+    name: "repo",
+  },
+  {
+    message: "Enter a brief description of the project. (Required)",
     type: "input",
     name: "description",
   },
-
+  
   {
     message: "What type of license, if any, is this project using?",
     type: "list",
     name: "license",
     choices: ['MIT', 'Apache 2.0', 'GNU V3', 'Mozilla 2.0', 'None']
+  },
+  {
+    message: "Please list any required packages for installation of your project.",
+    type: "input",
+    name: "installation",
+  },
+  {
+    message: "How will your project be used?",
+    type: "input",
+    name: "usage",
+  },
+  {
+    message: "Who contributed to this project?",
+    type: "input",
+    name: "contributors",
+  },
+  {
+    message: "Provide any applicable test information for your project.",
+    type: "input",
+    name: "tests",
+  },
+  {
+    message: "Please provide an email address for others to reach you at with questions",
+    type: "input",
+    name: "questions",
   },
 ];
 
@@ -39,11 +73,9 @@ function init() {
   .prompt(questions)
     .then((answers) => {
       const markdownString = generateMarkdown(answers);
-      console.log(markdownString);
       generateFile('test.md', markdownString);
   });
 }
 
 // Function call to initialize app
 init();
-
