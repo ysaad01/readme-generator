95f67f2b178f751d958d7d216566b2059fdd5f8f
ysaad01 <ysaad2325@gmail.com>
2022-04-09 13:02:52 -0400

    added require variables; added question objects; created generateFile function; created init function

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,49 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    message: "What is the title of your project?",
+    type: "input",
+    name: "title",
+  },
+
+  {
+    message: "Provide a description of the project:",
+    type: "input",
+    name: "description",
+  },
+
+  {
+    message: "What type of license, if any, is this project using?",
+    type: "list",
+    name: "license",
+    choices: ['MIT', 'Apache 2.0', 'GNU V3', 'Mozilla 2.0', 'None']
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function generateFile(fileName, data) {
+  fs.writeFile(fileName, data, 'utf8', (err) => {
+    if (err) throw err;
+    console.log("File was successfully created!");
+  })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+  .prompt(questions)
+    .then((answers) => {
+      const markdownString = generateMarkdown(answers);
+      console.log(markdownString);
+      generateFile('test.md', markdownString);
+  });
+}
 
 // Function call to initialize app
 init();
+
